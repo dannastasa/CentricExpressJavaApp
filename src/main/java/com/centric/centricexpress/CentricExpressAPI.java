794999543ecc6634014e43a0fdc68c76b0779d97
dannastasa <dan.nastasa20@gmail.com>
2017-02-20 09:59:21 +0200

    Added Swagger integration

--- src/main/java/com/centric/centricexpress/CentricExpressAPI.java
+++ src/main/java/com/centric/centricexpress/CentricExpressAPI.java
@@ -2,8 +2,10 @@ package com.centric.centricexpress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "com.centric.centricexpress")
+@Import(SwaggerConfig.class)
 public class CentricExpressAPI {
 
     public static void main(String[] args) {
