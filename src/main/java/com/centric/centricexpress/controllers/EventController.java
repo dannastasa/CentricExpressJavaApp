0ab200fa784b345d334d9c8a447203b3ad41eaef
Cristian Lucutar <cristian.lucutar@centric.eu>
2017-02-20 09:02:25 +0200

    Added base path for the API.

--- src/main/java/com/centric/centricexpress/controllers/EventController.java
+++ src/main/java/com/centric/centricexpress/controllers/EventController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -22,6 +23,7 @@ import com.centric.centricexpress.services.EventDayService;
 import com.centric.centricexpress.services.EventService;
 
 @Controller
+@RequestMapping("/api")
 public class EventController {
 
     @Autowired
